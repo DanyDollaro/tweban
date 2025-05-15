824486ea5e3ba866acfa681ec80fa3c651b714ff
Chris0451 <dcchris56@gmail.com>
2025-05-15 18:34:58 +0200

    Aggiornamento struttura home - parte 1
    Divisa lo home in sezioni principali per un accesso diretto alla modifica di determinate sezioni della pagina

---
+++ resources/views/home_layouts/navbar.blade.php
@@ -0,0 +1,25 @@
+<div class="branding d-flex align-items-center">
+
+    <div class="container position-relative d-flex align-items-center justify-content-between">
+        <a href="" class="logo d-flex align-items-center me-auto">
+          <!-- Uncomment the line below if you also wish to use an image logo -->
+          <!-- <img src="assets/img/logo.png" alt=""> -->
+          <h1 class="sitename">Medilab</h1>
+        </a>
+
+        <nav id="navmenu" class="navmenu">
+          <ul>
+            <li><a href="#hero" class="active">Home<br></a></li>
+            <li><a href="#about">Chi siamo</a></li>
+            <li><a href="#services">Servizi</a></li>
+            <li><a href="#contact">Orari e contatti</a></li>
+            <li><a href="#faq">FAQ</a></li>
+          </ul>
+          <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+        </nav>
+
+        <a class="cta-btn d-none d-sm-block" href="#appointment">Make an Appointment</a>
+
+    </div>
+
+</div>
