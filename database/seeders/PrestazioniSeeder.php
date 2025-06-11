384ae91cd0abbe0ccd9b2a3df80430568f495ce1
Chris0451 <dcchris56@gmail.com>
2025-06-11 14:39:24 +0200

    Rimosso vincolo tra prestazione e staff

--- database/seeders/PrestazioniSeeder.php
+++ database/seeders/PrestazioniSeeder.php
@@ -14,15 +14,15 @@ class PrestazioniSeeder extends Seeder
     public function run(): void
     {
         DB::table('prestazione')->insert([
-            ['tipologia' => 'Elettrocardiogramma','prescrizione' => 'prescrizione xxx', 'descrizione' => 'Descrizione XXX', 'sp_dipartimento'=>'Cardiologia', 'staff_id' =>'2'],
-            ['tipologia' => 'Visita cardiologica','prescrizione' => 'prescrizione xxx', 'descrizione' => 'Descrizione XXX', 'sp_dipartimento'=>'Cardiologia', 'staff_id' =>'2'],
-            ['tipologia' => 'Visita ortopedica','prescrizione' => 'prescrizione xxx', 'descrizione' => 'Descrizione XXX', 'sp_dipartimento'=>'Ortopedia', 'staff_id' =>'2'],
-            ['tipologia' => 'Radiografia articolare','prescrizione' => 'prescrizione xxx', 'descrizione' => 'Descrizione XXX', 'sp_dipartimento'=>'Ortopedia', 'staff_id' =>'2'],
-            ['tipologia' => 'Risonanza magnetica muscolo-scheletrica','prescrizione' => 'prescrizione xxx', 'descrizione' => 'Descrizione XXX', 'sp_dipartimento'=>'Ortopedia', 'staff_id' =>'2'],
-            ['tipologia' => 'Visita dermatologica','prescrizione' => 'prescrizione xxx', 'descrizione' => 'Descrizione XXX', 'sp_dipartimento'=>'Dermatologia', 'staff_id' =>'3'],
-            ['tipologia' => 'Dermatoscopia','prescrizione' => 'prescrizione xxx', 'descrizione' => 'Descrizione XXX', 'sp_dipartimento'=>'Dermatologia', 'staff_id' =>'3'],
-            ['tipologia' => 'Visita ginecologica','prescrizione' => 'prescrizione xxx', 'descrizione' => 'Descrizione XXX', 'sp_dipartimento'=>'Ginecologia', 'staff_id' =>'3'],
-            ['tipologia' => 'Visita neurologica','prescrizione' => 'prescrizione xxx', 'descrizione' => 'Descrizione XXX', 'sp_dipartimento'=>'Neurologia', 'staff_id' =>'3']
+            ['tipologia' => 'Elettrocardiogramma','prescrizione' => 'prescrizione xxx', 'descrizione' => 'Descrizione XXX', 'sp_dipartimento'=>'Cardiologia'],
+            ['tipologia' => 'Visita cardiologica','prescrizione' => 'prescrizione xxx', 'descrizione' => 'Descrizione XXX', 'sp_dipartimento'=>'Cardiologia'],
+            ['tipologia' => 'Visita ortopedica','prescrizione' => 'prescrizione xxx', 'descrizione' => 'Descrizione XXX', 'sp_dipartimento'=>'Ortopedia'],
+            ['tipologia' => 'Radiografia articolare','prescrizione' => 'prescrizione xxx', 'descrizione' => 'Descrizione XXX', 'sp_dipartimento'=>'Ortopedia'],
+            ['tipologia' => 'Risonanza magnetica muscolo-scheletrica','prescrizione' => 'prescrizione xxx', 'descrizione' => 'Descrizione XXX', 'sp_dipartimento'=>'Ortopedia'],
+            ['tipologia' => 'Visita dermatologica','prescrizione' => 'prescrizione xxx', 'descrizione' => 'Descrizione XXX', 'sp_dipartimento'=>'Dermatologia'],
+            ['tipologia' => 'Dermatoscopia','prescrizione' => 'prescrizione xxx', 'descrizione' => 'Descrizione XXX', 'sp_dipartimento'=>'Dermatologia'],
+            ['tipologia' => 'Visita ginecologica','prescrizione' => 'prescrizione xxx', 'descrizione' => 'Descrizione XXX', 'sp_dipartimento'=>'Ginecologia'],
+            ['tipologia' => 'Visita neurologica','prescrizione' => 'prescrizione xxx', 'descrizione' => 'Descrizione XXX', 'sp_dipartimento'=>'Neurologia']
         ]);
     }
 }
