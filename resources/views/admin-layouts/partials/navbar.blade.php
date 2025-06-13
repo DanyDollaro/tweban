c60885f9724d33abafdf7a8cb3522151561c1a27
DanyDollaro <danydo2603@gmail.com>
2025-06-13 03:00:15 +0200

    logout

--- resources/views/admin-layouts/partials/navbar.blade.php
+++ resources/views/admin-layouts/partials/navbar.blade.php
@@ -2,8 +2,11 @@
     <div class="header-title">
         {{ $title }}
     </div>
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+        @csrf
+    </form>
     <nav class="navbar">
-        <a href="/todo" class="nav-link">Logout</a>
+        <a href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="nav-link">Logout</a>
         <a href="/admin/analytics" class="nav-link">Analytics</a>
         <a href="/admin/dipartimenti" class="nav-link">Dipartimenti</a>
         <a href="/admin/personale" class="nav-link">Personale</a>
